ae60b59f7c378bd9d8b3a24fdf8928062a0a4487
codedwells <wells.mail.13@gmail.com>
2023-11-14 12:25:24 +0300

    Added shadcn

--- frontend/src/app/(home)/page.tsx
+++ frontend/src/app/(home)/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <main className='min-h-screen bg-gray-50'>
-      <div className='md:px-24 py-14'>
+      <div className='md:px-24 py-14 select-none'>
         <h1 className='scroll-m-20 text-4xl py-6 text-center font-extrabold tracking-tight lg:text-5xl'>
-          Elevate your agricultural experience.
+          Agricultural solutions reimagined.
         </h1>
 
         <h4 className='scroll-m-20 text-xl text-center text-gray-400 tracking-tight'>
