681679aa5058e5231bc9bf49f31116fda1633672
codedwells <wells.mail.13@gmail.com>
2023-11-15 21:07:03 +0300

    Added icons to navbar texts

--- frontend/src/components/layouts/ShopNav.tsx
+++ frontend/src/components/layouts/ShopNav.tsx
@@ -4,6 +4,9 @@ import { ThemeToggle } from '../buttons/ThemeToogle'
 import { Button } from '../ui/button'
 import Link from 'next/link'
 import { CartSheet } from '../modals/CartSheet'
+import { IoHomeOutline } from "react-icons/io5";
+import { MdOutlineLocalOffer } from "react-icons/md";
+import { FaRegHandshake } from "react-icons/fa";
 import {
   Select,
   SelectContent,
@@ -17,6 +20,7 @@ import {
 export const ShopNav = () => {
   return (
       <nav className='sticky z-50 h-16 top-0 backdrop:blur-lg flex bg-white shadow-sm flex-row items-center justify-between p-2 px-4 lg:px-72 dark:bg-gray-800'>
+      <Link href='/'>
         <Image
           draggable={false}
           src='/leaf_logo.svg'
@@ -26,8 +30,9 @@ export const ShopNav = () => {
           height={33}
           priority
         />
+        </Link>
 
-        <ul className='flex flex-col md:flex-row items-center space-x-4 text-sm'>
+        <ul className='flex items-center space-x-4 text-sm'>
 
           <Select>
             <SelectTrigger className='w-[180px]'>
@@ -47,17 +52,26 @@ export const ShopNav = () => {
 
           <Link
           href='/'
-          className='cursor-pointer hover:underline font-medium text-primary underline-offset-4'
+          className='hidden  md:flex items-center gap-1 cursor-pointer hover:underline font-medium text-primary underline-offset-4'
           >
+          <IoHomeOutline  />
+          <span>
           Home
+          </span>
           </Link>
 
-          <li className='cursor-pointer hover:underline font-medium text-primary underline-offset-4'>
+          <li className='hidden md:flex items-center gap-1 cursor-pointer hover:underline font-medium text-primary underline-offset-4'>
+          <MdOutlineLocalOffer />
+          <span>
             Offers
+            </span>
           </li>
 
-          <li className='cursor-pointer hover:underline font-medium text-primary underline-offset-4'>
+          <li className='hidden md:flex items-center gap-1 cursor-pointer hover:underline font-medium text-primary underline-offset-4'>
+          <FaRegHandshake size={18} />
+          <span>
             Terms
+            </span>
           </li>
         </ul>
 
