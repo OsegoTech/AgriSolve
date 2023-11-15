98426d736c374289dce4d5dd36aa841247aa65fe
codedwells <wells.mail.13@gmail.com>
2023-11-15 20:18:20 +0300

    Fixed Marketplace nav overflow

--- frontend/src/app/shop/page.tsx
+++ frontend/src/app/shop/page.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image'
+import {IoReload} from 'react-icons/io5'
 import { TfiShoppingCart } from 'react-icons/tfi'
+import { RiSecurePaymentLine } from "react-icons/ri";
+import { GoTrophy } from "react-icons/go";
 
 export default function Page() {
   return (
@@ -35,7 +38,6 @@ export default function Page() {
           priority
         />
       </div>
-
       <div className='w-full bg-white flex justify-between items-center p-4 rounded mt-8 '>
         <div className='flex items-center space-x-3 p-3'>
           <TfiShoppingCart size={36} className='text-orange-600' />
@@ -48,31 +50,31 @@ export default function Page() {
         </div>
 
         <div className='flex items-center space-x-3 p-3'>
-          <TfiShoppingCart size={36} className='text-orange-600' />
+          <IoReload size={36} className='text-orange-600' />
           <div className='space-y-1'>
-            <p className='text-xs font-medium'>Free Shipping</p>
+            <p className='text-xs font-medium'>Free Return</p>
             <p className='text-xs font-medium text-gray-400'>
-              On Orders Over $100
+              Get returns on viable orders
             </p>
           </div>
         </div>
 
         <div className='flex items-center space-x-3 p-3'>
-          <TfiShoppingCart size={36} className='text-orange-600' />
+          <RiSecurePaymentLine size={36} className='text-orange-600' />
           <div className='space-y-1'>
-            <p className='text-xs font-medium'>Free Shipping</p>
+            <p className='text-xs font-medium'>Secure Payments</p>
             <p className='text-xs font-medium text-gray-400'>
-              On Orders Over $100
+              100% Secure Online Payments
             </p>
           </div>
         </div>
 
         <div className='flex items-center space-x-3 p-3'>
-          <TfiShoppingCart size={36} className='text-orange-600' />
+          <GoTrophy size={36} className='text-orange-600' />
           <div className='space-y-1'>
-            <p className='text-xs font-medium'>Free Shipping</p>
+            <p className='text-xs font-medium'>Best Quality</p>
             <p className='text-xs font-medium text-gray-400'>
-              On Orders Over $100
+              Original Products Guaranteed
             </p>
           </div>
         </div>
