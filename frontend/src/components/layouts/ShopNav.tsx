5a3f8c04b98bab753c7d8b565115618f84e198f2
codedwells <wells.mail.13@gmail.com>
2023-11-15 14:58:30 +0300

    Added cart button

--- frontend/src/components/layouts/ShopNav.tsx
+++ frontend/src/components/layouts/ShopNav.tsx
@@ -1,40 +1,27 @@
-import Image from "next/image"
-import Link from "next/link"
-import { ThemeToggle } from "../buttons/ThemeToogle"
+import Image from 'next/image'
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { ThemeToggle } from '../buttons/ThemeToogle'
+import { Button } from '../ui/button';
 
 export const ShopNav = () => {
-    return (
+  return (
     <nav>
-<nav className='sticky z-50 h-16 top-0 backdrop:blur-lg flex bg-white shadow-sm flex-row items-center justify-between p-2 px-4 lg:px-8 dark:bg-gray-800'>
-      <Image
-        draggable={false}
-        src='/leaf_logo.svg'
-        alt='Leaf Logo'
-        className='dark:invert'
-        width={33}
-        height={33}
-        priority
-      />
+      <nav className='sticky z-50 h-16 top-0 backdrop:blur-lg flex bg-white shadow-sm flex-row items-center justify-between p-2 px-4 lg:px-8 dark:bg-gray-800'>
+        <Image
+          draggable={false}
+          src='/leaf_logo.svg'
+          alt='Leaf Logo'
+          className='dark:invert'
+          width={33}
+          height={33}
+          priority
+        />
 
-      <ul className='hidden md:flex space-x-4 text-sm'>
-      <li className='cursor-pointer'>Explore</li>
-        <Link
-          href='/shop'
-          className='cursor-pointer'
-        >
-          Shop
-        </Link>
-        <li className='cursor-pointer'>Forecasts</li>
- <li className='cursor-pointer'>Diseases</li>
-
-      </ul>
-
-      <ul className='flex space-x-2 text-sm'>
-
-        <ThemeToggle/>
-      </ul>
+        <ul className='flex space-x-2 text-sm'>
+        <Button><MdOutlineShoppingCart/></Button>
+          <ThemeToggle />
+        </ul>
+      </nav>
     </nav>
-
-    </nav>
-    )
+  )
 }
