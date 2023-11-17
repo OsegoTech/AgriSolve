b08d34c27ced7621cd425981f1fd8d66b0187063
codedwells <wells.mail.13@gmail.com>
2023-11-18 00:31:08 +0300

    Added home link to HomeNav bar

--- frontend/src/components/layouts/HomeNav.tsx
+++ frontend/src/components/layouts/HomeNav.tsx
@@ -9,40 +9,51 @@ import { Button } from '../ui/button'
 export default function HomeNav() {
   return (
     <nav className='sticky z-50 h-16 top-0 backdrop:blur-lg flex bg-white shadow-sm flex-row items-center justify-between p-2 px-4 lg:px-8 dark:bg-gray-800'>
-      <Image
-        draggable={false}
-        src='/leaf_logo.svg'
-        alt='Leaf Logo'
-        className='dark:invert'
-        width={33}
-        height={33}
-        priority
-      />
+      <Link href='/'>
+        <Image
+          draggable={false}
+          src='/leaf_logo.svg'
+          alt='Leaf Logo'
+          className='dark:invert'
+          width={33}
+          height={33}
+          priority
+        />
+      </Link>
 
       <ul className='hidden md:flex space-x-4 text-sm'>
-        <Link href='/shop' className='cursor-pointer flex items-center gap-1 hover:underline underline-offset-2'>
+        <Link
+          href='/shop'
+          className='cursor-pointer flex items-center gap-1 hover:underline underline-offset-2'
+        >
           <CiShop />
           <span>Shop</span>
         </Link>
 
-        <Link href='/forecast' className='cursor-pointer flex items-center gap-1 hover:underline underline-offset-2'>
+        <Link
+          href='/forecast'
+          className='cursor-pointer flex items-center gap-1 hover:underline underline-offset-2'
+        >
           <TiWeatherPartlySunny />
           <span>Forecasts</span>
         </Link>
 
-        <Link href="/disease-ai" className='cursor-pointer flex items-center gap-1 hover:underline underline-offset-2'>
+        <Link
+          href='/disease-ai'
+          className='cursor-pointer flex items-center gap-1 hover:underline underline-offset-2'
+        >
           <FaDisease />
           <span>Diseases</span>
         </Link>
       </ul>
 
       <ul className='flex space-x-2 text-sm'>
-      <Link href='/login'>
-       <Button variant="outline">Login</Button>
-       </Link>
-       <Link href='/register'>
-       <Button>Sign Up</Button>
-       </Link>
+        <Link href='/login'>
+          <Button variant='outline'>Login</Button>
+        </Link>
+        <Link href='/register'>
+          <Button>Sign Up</Button>
+        </Link>
         <ThemeToggle />
       </ul>
     </nav>
