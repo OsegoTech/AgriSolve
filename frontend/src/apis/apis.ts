cfbacd7755b7ca56a05a96fa308900100eed2610
codedwells <wells.mail.13@gmail.com>
2023-11-18 14:02:45 +0300

    Added weather stuff

---
+++ frontend/src/apis/apis.ts
@@ -0,0 +1,48 @@
+import axios from 'axios'
+
+const MICROSERVICE_URL = 'https://agri-solve-microservice.vercel.app'
+
+const usersApi = axios.create({
+  baseURL: `${MICROSERVICE_URL}/api/v1`,
+})
+
+export const getCurrentWeather = async ({
+  lat,
+  lon,
+}: {
+  lat: number
+  lon: number
+}) => {
+  try {
+    const { data } = await usersApi.post('/current_weather', {
+      latitude: lat,
+      longitude: lon,
+    })
+
+    return data;
+  } catch (err) {
+    console.log(err)
+  }
+
+}
+
+
+
+export const getForecastWeather = async ({
+  lat,
+  lon,
+}: {
+  lat: number
+  lon: number
+}) => {
+  try {
+    const { data } = await usersApi.post('/forecast', {
+      latitude: lat,
+      longitude: lon,
+    })
+
+    return data;
+  } catch (err) {
+    console.log(err)
+  }
+}
